580e6941917d746ca4dc0e15f6d65a4e7516ad0c
jtguggedal <jantore.guggedal@gmail.com>
2016-05-23 13:57:19 +0200

    Firmware

---
+++ Firmware/pca10040/s132/arm5_no_packs/Output/nrf52832_xxaa_s132 Debug/Obj/SEGGER_RTT_printf.d
@@ -0,0 +1,7 @@
+Output/nrf52832_xxaa_s132\ Debug/Obj/SEGGER_RTT_printf.o: \
+ c:\sdk\external\segger_rtt\segger_rtt_printf.c \
+ c:\sdk\external\segger_rtt\SEGGER_RTT.h \
+ c:\sdk\external\segger_rtt\SEGGER_RTT_Conf.h \
+ C:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ 2.16a/include/stdlib.h \
+ C:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ 2.16a/include/__crossworks.h \
+ C:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ 2.16a/include/stdarg.h
